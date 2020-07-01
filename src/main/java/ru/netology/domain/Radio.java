8e4c577b59067ef7b12199d951e01fe00bcf52b2
Oleg Georgiyevski <oleqg@yahoo.com>
2020-07-01 16:52:58 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int channelMax;
+    private int channelMin;
+    private int volumeMax;
+    private int volumeMin;
+    private int currentChannel;
+    private int currentVolume;
+}
